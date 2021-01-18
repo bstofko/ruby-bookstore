57614d5e7da57055299448fa31698f28580b4121
Bob Stofko <bob.stofko@gmail.com>
2021-01-18 11:22:53 -0600

    Added author search

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,12 +1,34 @@
 class Book < ApplicationRecord
   belongs_to :author
+  has_many :book_reviews
 
-  def self.search(query)
+  def self.search(query, title_only=false, book_format_physical:true)
+      
     if query
-        where(["title LIKE ?","%#{query}%"])
+      if title_only==true 
+        where(["title LIKE ?","%#{query.capitalize}%"])
+        #find_by title: "#{query.capitalize}".first
+
+      elsif title_only==false 
+        author = Author.where(["last_name = ?","#{query.capitalize}"]).take
+        if author
+          where(["author_id = ?", author.id])
+        # join books where author id
+        #pub = Publisher.where([{"name LIKE ?","%#{query}"}])
+        end 
+      #else
+      end
+      
     else
-        all
+      all
     end
-  end 
+  end
+
+  def self.test()
+    Author.all
+  end
+
+  def author_name
+  end
 
 end
