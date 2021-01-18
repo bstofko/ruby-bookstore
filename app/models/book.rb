c88562fff25c37a211dab5ee8ab42d851ade730c
Bob Stofko <bob.stofko@gmail.com>
2021-01-18 13:24:23 -0600

    Added author_name method

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   belongs_to :author
   has_many :book_reviews
 
-  def self.search(query, title_only=false, book_format_physical:true)
+  def self.search(query, title_only=true, book_format_physical:nil, book_format_type_id:nil)
       
     if query
       if title_only==true 
@@ -15,7 +15,6 @@ class Book < ApplicationRecord
         if author
           where(["author_id = ?", author.id])
         # join books where author id
-        #pub = Publisher.where([{"name LIKE ?","%#{query}"}])
         elsif publisher
           where(["publisher_id = ?", publisher.id])
         else
@@ -34,6 +33,12 @@ class Book < ApplicationRecord
   end
 
   def author_name
+    author = Author.find(author_id)
+    puts "#{author.last_name}, #{author.first_name}"
+  end
+
+  def average_rating
+
   end
 
 end
